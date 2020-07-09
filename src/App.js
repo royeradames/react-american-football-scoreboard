71d25b2e57d4b624e61993113be01e7124ffc5d2
royeradames <royeraadames@gmail.com>
2020-07-09 09:35:24 -0400

    Adding the footer to the bottom of the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-
+  const [homeScore, awayScore] = useState(0)
   return (
     <div className="container">
       <section className="scoreboard">
